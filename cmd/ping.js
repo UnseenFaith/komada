3d6b916effc23a2e88989bbe3739d799a21ed4d3
Evelyne Lachance <eslachance@gmail.com>
2016-10-04 22:53:53 -0400

    Removed dependency on performance-now, using msg timestamp instead

--- cmd/ping.js
+++ cmd/ping.js
@@ -1,16 +1,13 @@
-const now = require('performance-now');
 exports.run = (bot, msg, params = []) => {
-  var startTime = now();
-  msg.delete();
-  msg.channel.sendMessage("Let's see if this works")
-  .then( message => {
-    var endTime = now();
-    message.edit(`Ping took ${(endTime - startTime).toFixed(3)}ms. Test`).catch(console.error);
-  }).catch(console.error);
+  msg.channel.sendMessage('Ping?')
+  .then(message => {
+    message.edit(`Pong! (took: ${message.createdTimestamp - msg.createdTimestamp}ms)`);
+    msg.delete();
+  });
 };
 
 exports.help = {
   name : "ping",
-  description: "Ping/Pong. What do you *think* this does?",
+  description: "Ping/Pong command. I wonder what this does? /sarcasm",
   usage: "ping"
 };
