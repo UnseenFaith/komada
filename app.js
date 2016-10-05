6cadc189c401107891276f1f3496da09f3c1e1c6
Evelyne Lachance <eslachance@gmail.com>
2016-10-04 23:36:03 -0400

    Changed map() to forEach() so Gawdl3y could stop annoying me with performance

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({fetchAllMembers: true});
 const config = require('./config.json');
-const util = require("util");
 const fs = require('fs');
 const moment = require("moment");
 
