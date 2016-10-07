434e2c5b27a734ecff446d992a96fd56e083986e
Evelyne Lachance <eslachance@gmail.com>
2016-10-07 11:56:20 -0400

    Added QUIZ command example, and test command.

---
+++ cmd/test.js
@@ -0,0 +1,17 @@
+exports.run = (bot, msg, params = []) => {
+  console.log("Test Executed");
+  msg.reply("Test Executed");
+};
+
+exports.conf = {
+  enabled: false,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "test",
+  description: "This is a test command. What does it do? ",
+  usage: "test"
+};
