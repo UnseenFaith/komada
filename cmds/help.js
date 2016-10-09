9c22b0fecb077322d316b46585d3cb77a6a2e90b
Évelyne Lachance <eslachance@gmail.com>
2016-10-09 10:25:27 -0400

    Update help.js
    Small tweak to display the prefix everywhere (might as well)

--- cmds/help.js
+++ cmds/help.js
@@ -1,11 +1,11 @@
 exports.run = (bot, msg, params) => {
     if (!params[0]) {
-      msg.channel.sendCode("asciidoc", `= Command List =\n\n[Use ${bot.config.prefix}help <commandname> for details]\n\n${bot.commands.map(c=>`${c.help.name}:: ${c.help.description}`).join("\n")}`);
+      msg.channel.sendCode("asciidoc", `= Command List =\n\n[Use ${bot.config.prefix}help <commandname> for details]\n\n${bot.commands.map(c=>`${bot.config.prefix}${c.help.name} :: ${c.help.description}`).join("\n")}`);
   } else {
     let command = params[0];
     if(bot.commands.has(command)) {
       command = bot.commands.get(command);
-      msg.channel.sendCode("asciidoc", `= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}`);
+      msg.channel.sendCode("asciidoc", `= ${command.help.name} = \n${command.help.description}\nusage :: ${bot.config.prefix}${command.help.usage}`);
     }
   }
 };
