8e26fa6d27bb934a5544d32dc50dad3d973996c7
Simona <contact@cybersnets.com>
2016-10-09 23:17:23 +0900

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,8 +1,6 @@
-const config = require('../config.json');
-
 exports.run = (bot, msg, params) => {
     if (!params[0]) {
-      msg.channel.sendCode("asciidoc", `= Command List =\n\n[Use ${config.prefix}help <commandname> for details]\n\n${bot.commands.map(c=>`${c.help.name}:: ${c.help.description}`).join("\n")}`);
+      msg.channel.sendCode("asciidoc", `= Command List =\n\n[Use ${bot.config.prefix}help <commandname> for details]\n\n${bot.commands.map(c=>`${c.help.name}:: ${c.help.description}`).join("\n")}`);
   } else {
     let command = params[0];
     if(bot.commands.has(command)) {
