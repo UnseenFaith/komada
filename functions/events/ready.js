fbb66c43eae9ac4aad11cb8e408139fc19574858
bdistin <bdistin@gmail.com>
2016-10-11 10:40:19 -0500

    Update Ready to Komada

--- functions/events/ready.js
+++ functions/events/ready.js
@@ -1,3 +1,3 @@
 exports.run = (bot) => {
-  bot.log(`GuideBot: Ready to serve ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
+  bot.log(`Komada: Ready to serve ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
 };
