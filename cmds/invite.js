8c1482b8246bfd959b0b6040bcac0c05df38d36e
bdistin <bdistin@gmail.com>
2016-10-11 11:19:13 -0500

    Dynamic Invite Command (disabled in file for now)

---
+++ cmds/invite.js
@@ -0,0 +1,20 @@
+const clientID = require("../config.json").clientID;
+
+exports.help = {
+  name: "invite",
+  description: "Displays the join server link of the bot.",
+  usage: "?invite"
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0,
+  botPerms: []
+};
+
+exports.run = (bot, msg) => {
+  bot.log("Join Server Link");
+  msg.channel.sendMessage(`To add ${bot.user.username} to your discord guild: \n https://discordapp.com/oauth2/authorize?&client_id=${clientID}&scope=bot&permissions=${bot.functions.core.botPermissions(bot)}\n\`\`\`The above link is generated requesting the minimum permissions required to use every command currently. I know not all permissions are right for every server, so don't be afraid to uncheck any of the boxes. If you try to use a command that requires more permissions than the bot is granted, it will let you know.\`\`\`\nPlease file an issue at <https://github.com/eslachance/komada> if you find any bugs.`);
+};
