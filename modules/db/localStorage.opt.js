a664dab1650424994ee0c5181c3b43cb2703c516
bdistin <bdistin@gmail.com>
2016-10-12 18:35:40 -0500

    Ensures Database Directory exists, and makes it if it doesn't on init

--- modules/db/localStorage.opt.js
+++ modules/db/localStorage.opt.js
@@ -1,5 +1,5 @@
 const LocalStorage = require("node-localstorage").LocalStorage;
-const fs = require("fs");
+const fs = require("fs-extra");
 const tables = [];
 
 const config = {
@@ -12,26 +12,30 @@ exports.conf = config;
 exports.debug = client => {
   client.log(tables);
 };
+
 exports.init = (client) => {
   return new Promise( (resolve, reject) => {
-    try {
-      let test = new LocalStorage(`${config.baseLocation}/system-test`);
-      test.clear();
-    }
-    catch (e) {
-      console.log(e);
-      reject(e);
-    }
-    fs.readdir(config.baseLocation, (err, files) => {
-      if (err) console.error(err);
-      let c = 0;
-      files.forEach(f=> {
-        let name = f.split(".")[0];
-        tables[name] = new LocalStorage(`${config.baseLocation}/${name}`);
-        c++;
+    fs.ensureDir(config.baseLocation, (e) => {
+      if (e) console.error(e);
+      try {
+        let test = new LocalStorage(`${config.baseLocation}/system-test`);
+        test.clear();
+      } catch (e) {
+        console.log(e);
+        reject(e);
+      }
+      fs.readdir(config.baseLocation, (err, files) => {
+        if (err) console.error(err);
+        let c = 0;
+        files.forEach(f=> {
+          let name = f.split(".")[0];
+          tables[name] = new LocalStorage(`${config.baseLocation}/${name}`);
+          c++;
+        });
+        client.log(`Loaded ${c} tables in ${config.moduleName} database.`);
       });
-      client.log(`Loaded ${c} tables in ${config.moduleName} database.`);
     });
+
   });
 };
 
