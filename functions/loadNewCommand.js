22daec14bc9a3bdac265b48551c3d4db2536d966
bdistin <bdistin@gmail.com>
2016-10-22 23:39:59 -0500

    Fix and improve download

--- functions/loadNewCommand.js
+++ functions/loadNewCommand.js
@@ -2,7 +2,7 @@ module.exports = function(client, command) {
   return new Promise((resolve, reject) => {
     try {
       let cmd = require(`${client.clientBaseDir}commands/${command}`);
-      cmd.help["category"] = command.split("/").slice(0,-1).join("/");
+      cmd.help["category"] = `${client.clientBaseDir}commands/${command}`.split("/").slice(0,-1).join("/");
 
       client.commands.set(cmd.help.name, cmd);
       cmd.conf.aliases.forEach(alias => {
