8d99fb59bec981e0125e18e3a88377037b5bc3b0
Evelyne Lachance <eslachance@gmail.com>
2016-10-23 20:55:20 -0400

    Client files now override core files
    When loaded on boot.

--- functions/loadCommands.js
+++ functions/loadCommands.js
@@ -4,16 +4,20 @@ const path = require("path");
 module.exports = client => {
   client.commands.clear();
   client.aliases.clear();
-
-  loadCommands(client, client.coreBaseDir);
-  loadCommands(client, client.clientBaseDir);
+  let counts = [0, 0];
+  loadCommands(client, client.coreBaseDir, counts).then(counts => {
+    loadCommands(client, client.clientBaseDir, counts).then(counts => {
+      let [c, a] = counts;
+      client.funcs.log(`Loaded ${c} commands, with ${a} aliases.`);
+    });
+  });
 };
 
-const loadCommands = (client, baseDir) => {
+const loadCommands = (client, baseDir, counts) => {
   return new Promise( (resolve, reject) => {
     let dir = path.resolve(baseDir + "./commands/");
 
-    let [c, a] = [0,0];
+    let [c, a] = counts;
     try {
       fse.walk(dir)
       .on("data", (item) => {
@@ -35,8 +39,8 @@ const loadCommands = (client, baseDir) => {
         });
       })
       .on("end", () => {
-        client.funcs.log(`Loaded ${c} commands, with ${a} aliases.`);
-        resolve(`Loaded ${c} commands, with ${a} aliases.`);
+        //client.funcs.log(`Loaded ${c} commands, with ${a} aliases.`);
+        resolve([c, a]);
       });
     } catch (e) {
       if (e.code === "MODULE_NOT_FOUND") {
