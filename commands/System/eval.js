719b167fcb9277b1fba89a5903b9cb894c143d6d
Johnson Chen <CyberiumShadow@users.noreply.github.com>
2016-10-24 23:00:11 +1100

    Fixed Eval Inspection + added regex
    Fixed large Evals not being resolved due to inspect being defaulted to infinity.
    
    Sensitive Pattern regex was added to remove sensitive data like tokens, emails and passwords.

--- commands/System/eval.js
+++ commands/System/eval.js
@@ -1,9 +1,13 @@
+const escapeRegex = require('escape-string-regexp');
+
 exports.run = (client, msg, [code]) => {
   try {
     var evaled = eval(code);
     if (typeof evaled !== "string")
-      evaled = require("util").inspect(evaled);
-    msg.channel.sendCode("xl",client.funcs.clean(evaled));
+      evaled = require("util").inspect(evaled, {
+        depth: 0
+      }).replace(sensitivePattern(client), '---removed---').replace(client.user.email, '---removed---');
+    msg.channel.sendCode("xl", client.funcs.clean(evaled));
   } catch (err) {
     msg.channel.sendMessage("`ERROR` ```xl\n" +
       client.funcs.clean(err) +
@@ -25,4 +29,16 @@ exports.help = {
   description: "Evaluates arbitrary Javascript. Reserved for bot owner.",
   usage: "<expression:str>",
   usageDelim: ""
+};
+
+function sensitivePattern(client) {
+  if (!this._sensitivePattern) {
+    let pattern = '';
+    if (client.token) pattern += escapeRegex(client.token);
+    if (client.token) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.token);
+    if (client.email) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.email);
+    if (client.password) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.password);
+    this._sensitivePattern = new RegExp(pattern, 'gi');
+  }
+  return this._sensitivePattern;
 };
