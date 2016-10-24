c64b94403bc25e04abd8fa5dd21663e3ef845f43
Johnson Chen <CyberiumShadow@users.noreply.github.com>
2016-10-25 00:09:20 +1100

    Removed Required Module
    Removed 'POS' module as per Evie's request

--- commands/System/eval.js
+++ commands/System/eval.js
@@ -1,12 +1,10 @@
-const escapeRegex = require('escape-string-regexp');
-
 exports.run = (client, msg, [code]) => {
   try {
     var evaled = eval(code);
     if (typeof evaled !== "string")
       evaled = require("util").inspect(evaled, {
         depth: 0
-      }).replace(sensitivePattern(client), '---removed---').replace(client.user.email, '---removed---');
+      });
     msg.channel.sendCode("xl", client.funcs.clean(evaled));
   } catch (err) {
     msg.channel.sendMessage("`ERROR` ```xl\n" +
@@ -29,16 +27,4 @@ exports.help = {
   description: "Evaluates arbitrary Javascript. Reserved for bot owner.",
   usage: "<expression:str>",
   usageDelim: ""
-};
-
-function sensitivePattern(client) {
-  if (!this._sensitivePattern) {
-    let pattern = '';
-    if (client.token) pattern += escapeRegex(client.token);
-    if (client.token) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.token);
-    if (client.email) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.email);
-    if (client.password) pattern += (pattern.length > 0 ? '|' : '') + escapeRegex(client.password);
-    this._sensitivePattern = new RegExp(pattern, 'gi');
-  }
-  return this._sensitivePattern;
 };
