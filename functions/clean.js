d672f5c1a7b6424850805d2cf3b0dc5ca98e0396
Johnson Chen <CyberiumShadow@users.noreply.github.com>
2016-10-25 01:29:46 +1100

    Moved Sensitive Patterns to Clean Function

--- functions/clean.js
+++ functions/clean.js
@@ -1,7 +1,19 @@
-module.exports = (text) => {
+module.exports = (text, client) => {
   if (typeof(text) === "string") {
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    return text.replace(sensitivePattern(client), "「ｒｅｄａｃｔｅｄ」").replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));;
   } else {
     return text;
   }
+};
+
+function sensitivePattern(client) {
+  if (!this._sensitivePattern) {
+    let pattern = '';
+    if (client.token) pattern += client.token;
+    if (client.token) pattern += (pattern.length > 0 ? '|' : '') + client.token;
+    if (client.email) pattern += (pattern.length > 0 ? '|' : '') + client.email;
+    if (client.password) pattern += (pattern.length > 0 ? '|' : '') + client.password;
+    this._sensitivePattern = new RegExp(pattern, 'gi');
+  }
+  return this._sensitivePattern;
 };
