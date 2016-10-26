9460fe42ebcbcd5bd63f16ab3412d4606556b737
Évelyne Lachance <eslachance@gmail.com>
2016-10-26 00:21:36 -0400

    client comes first.

--- functions/clean.js
+++ functions/clean.js
@@ -1,4 +1,4 @@
-module.exports = (text, client) => {
+module.exports = (client, text) => {
   if (typeof(text) === "string") {
     return text.replace(sensitivePattern(client), "「ｒｅｄａｃｔｅｄ」").replace(client.user.email, '「ｒｅｄａｃｔｅｄ」').replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
   } else {
