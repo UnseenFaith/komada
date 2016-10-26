db51117986f76637309f2a22c76ddfee68829093
Évelyne Lachance <eslachance@gmail.com>
2016-10-26 00:21:16 -0400

    SPACING. 2 spaces please.

--- commands/System/eval.js
+++ commands/System/eval.js
@@ -1,29 +1,29 @@
 exports.run = (client, msg, [code]) => {
-    try {
-      var evaled = eval(code);
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-      msg.channel.sendCode("xl", client.funcs.clean(client, evaled));
-      }
-      catch (err) {
-        msg.channel.sendMessage("`ERROR` ```xl\n" +
-          client.funcs.clean(err) +
-          "\n```");
-      }
-    };
+  try {
+    var evaled = eval(code);
+    if (typeof evaled !== "string")
+      evaled = require("util").inspect(evaled);
+    msg.channel.sendCode("xl", client.funcs.clean(client, evaled));
+  }
+  catch (err) {
+    msg.channel.sendMessage("`ERROR` ```xl\n" +
+      client.funcs.clean(err) +
+      "\n```");
+  }
+};
 
-    exports.conf = {
-      enabled: true,
-      guildOnly: false,
-      aliases: ["ev"],
-      permLevel: 10,
-      botPerms: [],
-      requiredFuncs: []
-    };
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["ev"],
+  permLevel: 10,
+  botPerms: [],
+  requiredFuncs: []
+};
 
-    exports.help = {
-      name: "eval",
-      description: "Evaluates arbitrary Javascript. Reserved for bot owner.",
-      usage: "<expression:str>",
-      usageDelim: ""
-    };
+exports.help = {
+  name: "eval",
+  description: "Evaluates arbitrary Javascript. Reserved for bot owner.",
+  usage: "<expression:str>",
+  usageDelim: ""
+};
