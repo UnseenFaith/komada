ad48002c691ef0c091f74988076e8ea9bdfa2d18
Faith <tba28012@hotmail.com>
2016-10-29 09:16:58 -0400

    Custom Permission Roles Support

--- functions/permissionLevel.js
+++ functions/permissionLevel.js
@@ -1,13 +1,14 @@
 module.exports = (client, user, guild = null) => {
   return new Promise((resolve, reject) => {
+    let guildConf = client.funcs.confs.get(guild);
     let permlvl = 0;
     if(guild) {
-      try{
+      try {
         let member = guild.member(user);
-        let mod_role = guild.roles.find("name", "Mods");
+        let mod_role = guild.roles.find("name", guildConf.mod_role);
         if (mod_role && member.roles.has(mod_role.id))
           permlvl = 2;
-        let admin_role = guild.roles.find("name", "Devs");
+        let admin_role = guild.roles.find("name", guildConf.admin_role);
         if (admin_role && member.roles.has(admin_role.id))
           permlvl = 3;
         if(member === guild.owner)
