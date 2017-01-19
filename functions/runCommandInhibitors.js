f30affee2de32428e09e5cb573140aad43cf7cbc
Faith <tba28012@hotmail.com>
2017-01-19 21:01:43 +0100

    [PR] Fix for My Idiotic Mistake

--- functions/runCommandInhibitors.js
+++ functions/runCommandInhibitors.js
@@ -5,7 +5,7 @@ module.exports = (client, msg, cmd, selective = false) => new Promise((resolve, 
   client.commandInhibitors.forEach((mProc, key) => {
     if (key === "usage") usage = i;
     if (!mProc.conf.spamProtection || !selective) {
-      mps.push(mProc.run(client, msg, cmd));
+      mps.push(mProc.run(client, msg, cmd, args));
     }
     i++;
   });
