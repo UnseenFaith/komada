ae146d47f930468713a422746a49a99906f4ec86
Faith <tba28012@hotmail.com>
2017-01-20 02:58:40 +0100

    [PR] Prefix.length error fixed

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ exports.start = (config) => {
     } else if (msg.content.startsWith(conf.prefix)) {
       thisPrefix = conf.prefix;
     }
-    if (!thisPrefix && (client.config.prefixMention && !client.config.prefixMention.test(msg.content))) return;
+    if (!thisPrefix) return;
+    if (client.config.prefixMention && !client.config.prefixMention.test(msg.content)) return;
     let prefixLength = thisPrefix.length;
     if (client.config.prefixMention && client.config.prefixMention.test(msg.content)) prefixLength = client.config.prefixMention.exec(msg.content)[0].length + 1;
     const command = msg.content.slice(prefixLength).split(" ")[0].toLowerCase();
