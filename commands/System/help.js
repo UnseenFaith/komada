2ed26979d98963a3205fd97c0cc9446851c0e766
Johnson Chen <CyberiumShadow@users.noreply.github.com>
2017-01-21 17:22:12 +1100

    Fixed selfbot's not being able to DM themselves

--- commands/System/help.js
+++ commands/System/help.js
@@ -11,12 +11,19 @@ exports.run = (client, msg, [cmd]) => {
           }
           helpMessage.push("```\n\u200b");
         }
-        msg.author.sendMessage(helpMessage, { split: { char: "\u200b" } }).catch((e) => { console.error(e); });
-        msg.reply('Commands have been sent to your DMs.');
+        if (!client.config.selfbot) {
+        	msg.author.sendMessage(helpMessage, { split: { char: "\u200b" } }).catch((e) => { console.error(e); });
+        	msg.reply('Commands have been sent to your DMs.');
+				} else {msg.channel.sendMessage(helpMessage, { split: { char: "\u200b" } })
+        .catch((e) => { console.error(e); })};
       });
   } else if (client.commands.has(cmd)) {
     cmd = client.commands.get(cmd);
-    msg.author.sendCode("asciidoc", `= ${cmd.help.name} = \n${cmd.help.description}\nusage :: ${client.funcs.fullUsage(client, cmd)}`);
+    if (!client.config.selfbot) {
+        msg.author.sendCode("asciidoc", `= ${cmd.help.name} = \n${cmd.help.description}\nusage :: ${client.funcs.fullUsage(client, cmd)}`);
+    } else {
+        msg.channel.sendCode("asciidoc", `= ${cmd.help.name} = \n${cmd.help.description}\nusage :: ${client.funcs.fullUsage(client, cmd)}`);
+    }
   }
 };
 
