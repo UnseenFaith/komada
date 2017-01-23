69d0175955dad46366cdf0c693d1b78dcc36d267
Faith <tba28012@hotmail.com>
2017-01-23 19:24:41 +0100

    Bunch of Bugfixes

--- functions/loadProviders.js
+++ functions/loadProviders.js
@@ -7,7 +7,7 @@ const loadProviders = (client, baseDir) => new Promise(async (resolve, reject) =
   const files = await client.funcs.getFileListing(client, baseDir, "providers").catch(err => client.funcs.log(err, "error"));
   try {
     files.forEach((f) => {
-      const props = require(`${dir}/${f}`);
+      const props = require(`${dir}${path.sep}${f}`);
       client.providers.set(f.name, props);
     });
     resolve();
