bbfb5d5d9e826a07eb0d29e96380221cd9a8e914
Faith <tba28012@hotmail.com>
2017-01-29 13:10:06 +0100

    Potential Inhib fix

--- functions/runCommandInhibitors.js
+++ functions/runCommandInhibitors.js
@@ -1,18 +1,26 @@
 module.exports = (client, msg, cmd, args, selective = false) => new Promise((resolve, reject) => {
   let usage;
+  let error;
   const priority = client.commandInhibitors.array();
   const sorted = priority.sort((a, b) => a.conf.priority < b.conf.priority);
-  sorted.some(async (inhib) => { // eslint-disable-line
-    if (!selective && inhib.conf.enabled) {
-      await inhib.run(client, msg, cmd, args)
-      .then((value) => {
-        if (value) usage = value;
-      })
-      .catch((error) => {
-        reject(error);
-        return true;
-      });
+  sorted.some((inhib) => { // eslint-disable-line
+    if (!msg.cmdInhibited) {
+      if (!selective && inhib.conf.enabled) {
+        inhib.run(client, msg, cmd, args)
+        .then((value) => {
+          if (value) usage = value;
+        })
+        .catch((err) => {
+          if (err) {
+            error = err;
+            msg.cmdInhibited = true;
+          }
+        });
+      }
+    } else {
+      return true;
     }
   });
-  setTimeout(() => { resolve(usage); }, 1);
+  if (msg.cmdInhibited) return setTimeout(() => { reject(error); }, 1);
+  return setTimeout(() => { resolve(usage); }, 1);
 });
