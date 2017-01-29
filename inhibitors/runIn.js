50eb143fbbca6658e0a207de8fe854da902317e7
Johnson Chen <CyberiumShadow@users.noreply.github.com>
2017-01-30 10:16:08 +1100

    Sounds better

--- inhibitors/runIn.js
+++ inhibitors/runIn.js
@@ -8,6 +8,6 @@ exports.run = (client, msg, cmd) => new Promise((resolve, reject) => {
   if (cmd.conf.runIn.includes(msg.channel.type)) {
     resolve();
   } else {
-    reject(`This command is only avaliable in ${cmd.conf.runIn.join(" ")}`);
+    reject(`This command is only avaliable in ${cmd.conf.runIn.join(" ")} channels`);
   }
 });
