f2ae17021265ed4b66cde9e568b228a2cedc70bb
Faith <tba28012@hotmail.com>
2017-01-29 16:22:25 -0500

    Inhibitor Fix (#130)
    * Inhibitor Fix
    
    * Removed useless async
    
    * [PR] Help Fix

--- functions/runCommandInhibitors.js
+++ functions/runCommandInhibitors.js
@@ -1,26 +1,20 @@
 module.exports = (client, msg, cmd, args, selective = false) => new Promise((resolve, reject) => {
   let usage;
-  let error;
   const priority = client.commandInhibitors.array();
   const sorted = priority.sort((a, b) => a.conf.priority < b.conf.priority);
   sorted.some((inhib) => { // eslint-disable-line
-    if (!msg.cmdInhibited) {
-      if (!selective && inhib.conf.enabled) {
-        inhib.run(client, msg, cmd, args)
-        .then((value) => {
-          if (value) usage = value;
-        })
-        .catch((err) => {
-          if (err) {
-            error = err;
-            msg.cmdInhibited = true;
-          }
-        });
-      }
-    } else {
-      return true;
+    if (!selective && inhib.conf.enabled) {
+      inhib.run(client, msg, cmd, args)
+      .then((params) => {
+        if (params) usage = params;
+      })
+      .catch((err) => {
+        if (err) {
+          reject(err);
+          return true;
+        }
+      });
     }
   });
-  if (msg.cmdInhibited) return setTimeout(() => { reject(error); }, 1);
   return setTimeout(() => { resolve(usage); }, 1);
 });
