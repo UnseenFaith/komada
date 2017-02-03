5d26aff10347d00ca84054abbdac1d89e9cb495b
Faith <tba28012@hotmail.com>
2017-02-03 05:16:33 +0100

    [PR] Eslint && Array-prefixing fix

--- functions/handleCommand.js
+++ functions/handleCommand.js
@@ -23,19 +23,18 @@ exports.getCommand = (client, command) => {
     return client.commands.get(command);
   } else if (client.aliases.has(command)) {
     return client.commands.get(client.aliases.get(command));
-  } else {
-    return false;
   }
+  return false;
 };
 
 exports.runInhibitors = (client, msg, command) => {
   const priority = client.commandInhibitors.array().sort((low, high) => low.conf.priority < high.conf.priority);
   let response;
-  priority.some((inhibitor) => {
+  priority.some((inhibitor) => { // eslint-disable-line
     if (inhibitor.conf.enabled) {
       response = inhibitor.run(client, msg, command);
       if (response) return true;
     }
   });
   return response;
-}
+};
--- functions/parseCommand.js
+++ functions/parseCommand.js
@@ -4,27 +4,27 @@ module.exports = (client, msg, usage = false) => {
   const prefixLength = this.getLength(client, msg, prefix);
   if (usage) return prefixLength;
   return msg.content.slice(prefixLength).split(" ")[0].toLowerCase();
-}
+};
 
 exports.getLength = (client, msg, prefix) => {
   if (client.config.prefixMention === prefix) {
     return prefix.exec(msg.content)[0].length + 1;
   }
   return prefix.exec(msg.content)[0].length;
-}
+};
 
 exports.getPrefix = (client, msg) => {
   if (client.config.prefixMention.test(msg.content)) {
     return client.config.prefixMention;
-  } else {
-    const prefix = msg.guildConf.prefix;
-    if (prefix instanceof Array) {
-      prefix.forEach((prefix) => {
-        if (msg.content.startsWith(prefix)) return new RegExp(`^${prefix}`);
-      });
-    } else {
-      if (msg.content.startsWith(prefix)) return new RegExp(`^${prefix}`);
-    }
   }
+  const prefix = msg.guildConf.prefix;
+  if (prefix instanceof Array) {
+    prefix.forEach((prefix) => {
+      if (msg.content.startsWith(prefix)) return new RegExp(`^${prefix}`);
+      return false;
+    });
+    return prefix;
+  }
+  if (msg.content.startsWith(prefix)) return new RegExp(`^${prefix}`); // eslint-disable-line
   return false;
-}
+};
