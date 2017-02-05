6c24fdb23754b5c167287e379dec06f32fce163e
Johnson Chen <CyberiumShadow@users.noreply.github.com>
2017-02-06 09:48:31 +1100

    Continuation of #141

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ exports.start = async (config) => {
 
   client.once("ready", async () => {
     client.config.prefixMention = new RegExp(`^<@!?${client.user.id}>`);
-    await client.funcs.initialize(client);
     client.destroy = () => "You cannot use this within Komada, use process.exit() instead.";
   });
 
