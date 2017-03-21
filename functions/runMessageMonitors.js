c6a9bdc974876dc7569c2c601daaf63689a60222
Faith <tba28012@hotmail.com>
2017-03-21 15:35:29 -0400

    oops

--- functions/runMessageMonitors.js
+++ functions/runMessageMonitors.js
@@ -1,8 +1,8 @@
 module.exports = (client, msg) => {
   client.messageMonitors.forEach((monit) => {
     if (monit.conf.enabled) {
-      if (monit.ignoreBots && msg.author.bot) return;
-      if (monit.ignoreSelf && client.user === msg.author) return;
+      if (monit.conf.ignoreBots && msg.author.bot) return;
+      if (monit.conf.ignoreSelf && client.user === msg.author) return;
       monit.run(client, msg);
     }
   });
