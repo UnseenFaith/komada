6568ec23564b271ec71c7c48c53b8a521feeacd6
Faith <tba28012@hotmail.com>
2017-03-21 16:08:19 -0400

    Monitor Changes && Usage Fix (#165)
    * Monitor Changes && Usage Fix
    
    * oops

--- functions/usage.js
+++ functions/usage.js
@@ -8,14 +8,13 @@ exports.conf = {
 
 exports.run = (client, msg, cmd, args = undefined) => new Promise((resolve, reject) => {
   const usage = client.funcs.parseUsage(cmd.help.usage);
-  let prefixLength = msg.guildConf.prefix.length;
-  if (client.config.prefixMention.test(msg.content)) prefixLength = client.config.prefixMention.exec(msg.content)[0].length + 1;
+  const prefixLength = client.funcs.parseCommand(client, msg, true);
   if (args === undefined) {
     args = msg.content.slice(prefixLength)
     .split(" ")
     .slice(1)
     .join(" ")
-    .split(cmd.help.usageDelim !== "" ? cmd.help.usageDelim : null);
+    .split(cmd.help.usageDelim !== "" ? cmd.help.usageDelim : undefined);
   }
   if (args[0] === "") args = [];
   let currentUsage;
