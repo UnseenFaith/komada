90d0b32831af56547c44fd3a2163ac100ba2c318
Faith <tba28012@hotmail.com>
2017-04-01 21:11:04 -0400

    Add to all configs, not just default

--- classes/Config.js
+++ classes/Config.js
@@ -174,6 +174,9 @@ class Config {
     if (!key || !defaultMinValue) return `You supplied ${key}, ${defaultMinValue}. Please supply both a key, and a default min value.`;
     if (defaultConf[key].type !== "Number") return "You cannot use this method on non-Numeral configurations.";
     defaultConf[key].min = defaultMinValue;
+    guildConfs.forEach((config) => {
+      config[key].setMin(defaultMinValue);
+    });
     fs.outputJSONAsync(`${dataDir}${path.sep}${defaultFile}`, defaultConf);
     return defaultConf[key];
   }
@@ -188,7 +191,10 @@ class Config {
   static setMax(key, defaultMaxValue) {
     if (!key || !defaultMaxValue) return `You supplied ${key}, ${defaultMaxValue}. Please supply both a key, and a default max value.`;
     if (defaultConf[key].type !== "Number") return "You cannot use this method on non-Numeral configurations.";
-    defaultConf[key].min = defaultMaxValue;
+    defaultConf[key].max = defaultMaxValue;
+    guildConfs.forEach((config) => {
+      config[key].setMax(defaultMaxValue);
+    });
     fs.outputJSONAsync(`${dataDir}${path.sep}${defaultFile}`, defaultConf);
     return defaultConf[key];
   }
@@ -205,6 +211,9 @@ class Config {
     if (defaultConf[key].type !== "Array") return "You cannot use this method on non-Array configuration options.";
     if (defaultConf[key].data.includes(defaultValue)) return `The default value ${defaultValue} is already in ${defaultConf[key].data}.`;
     defaultConf[key].data.push(defaultValue);
+    guildConfs.forEach((config) => {
+      config[key].add(defaultValue);
+    });
     fs.outputJSONAsync(`${dataDir}${path.sep}${defaultFile}`, defaultConf);
     return defaultConf[key];
   }
@@ -221,6 +230,9 @@ class Config {
     if (defaultConf[key].type !== "Array") return "You cannot use this method on non-Array configuration options.";
     if (!defaultConf[key].data.includes(defaultValue)) return `The default value ${defaultValue} is not in ${defaultConf[key].data}.`;
     defaultConf[key].data.splice(defaultConf[key].indexOf(defaultValue), 1);
+    guildConfs.forEach((config) => {
+      config[key].del(defaultValue);
+    });
     fs.outputJSONAsync(`${dataDir}${path.sep}${defaultFile}`, defaultConf);
     return defaultConf[key];
   }
@@ -236,6 +248,9 @@ class Config {
     if (defaultConf[key].type !== "Boolean") return "You cannot use this method on non-Boolean configuration options.";
     if (defaultConf[key].data === true) defaultConf[key].data = false;
     else defaultConf[key].data = false;
+    guildConfs.forEach((config) => {
+      config[key].toggle();
+    });
     fs.outputJSONAsync(`${dataDir}${path.sep}${defaultFile}`, defaultConf);
     return defaultConf[key];
   }
