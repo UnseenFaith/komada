9e6a8daa24fb909ae479becaa9d2799355aff0ed
bdistin <bdistin@gmail.com>
2017-04-15 09:36:39 -0500

    clean up

--- events/message.js
+++ events/message.js
@@ -1,8 +1,18 @@
 exports.run = async (client, msg) => {
 	if (!client.ready) return;
-	await client.funcs.runMessageMonitors(client, msg);
+	await this.runMessageMonitors(client, msg);
 	client.i18n.use(msg.guildConf.lang);
 	if (!client.funcs.handleMessage(client, msg)) return;
 	const command = client.funcs.parseCommand(client, msg);
 	client.funcs.handleCommand(client, msg, command);
+};
+
+exports.runMessageMonitors = (client, msg) => {
+	client.messageMonitors.forEach((monit) => {
+		if (monit.conf.enabled) {
+			if (monit.conf.ignoreBots && msg.author.bot) return;
+			if (monit.conf.ignoreSelf && client.user === msg.author) return;
+			monit.run(client, msg);
+		}
+	});
 };
