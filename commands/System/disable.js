ba990cec6ad8f7bd175b8b80bf8d604b5aab22ef
bdistin <bdistin@gmail.com>
2017-04-14 23:17:22 -0500

    enable/disable (#192)
    * Update enable.js
    
    * Update disable.js
    
    * fix lint on disable
    
    * fix enable lint
    
    * take 2 fix lint disable
    
    * take 2 fix lint enable

--- commands/System/disable.js
+++ commands/System/disable.js
@@ -1,42 +1,25 @@
-/* eslint-disable no-case-declarations, consistent-return */
 exports.run = (client, msg, [type, name]) => {
   switch (type) {
-    case "inhibitor":
-      let inhibitor;
-      if (client.commandInhibitors.has(name)) {
-        inhibitor = name;
-      }
-      if (!inhibitor) {
-        msg.channel.sendCode("diff", `- I cannot find the inhibitor: ${name}`);
-      }
-      client.commandInhibitors.get(inhibitor).conf.enabled = false;
-      msg.channel.sendCode("diff", `+ Successfully disabled inhibitor: ${name}`);
-      break;
-    case "monitor":
-      let monitor;
-      if (client.messageMonitors.has(name)) {
-        monitor = name;
-      }
-      if (!monitor) {
-        return msg.channel.sendCode("diff", `- I cannot find the monitor: ${name}`);
-      }
-      client.messageMonitors.get(monitor).conf.enabled = false;
-      msg.channel.sendCode("diff", `+ Successfully disabled monitor: ${name}`);
-      break;
-    case "command":
-      let command;
-      if (client.commands.has(name)) {
-        command = name;
-      } else if (client.aliases.has(name)) {
-        command = client.aliases.get(name);
-      }
-      if (!command) {
-        return msg.channel.sendCode("diff", `- I cannot find the command: ${name}`);
-      }
-      client.commands.get(command).conf.enabled = false;
-      msg.channel.sendCode("diff", `+ Successfully disabled command: ${name}`);
-      break;
-    // no default
+    case "inhibitor": {
+      const inhibitor = client.commandInhibitors.get(name);
+      if (!inhibitor) return msg.channel.sendCode("diff", `- I cannot find the inhibitor: ${name}`);
+      inhibitor.conf.enabled = false;
+      return msg.sendCode("diff", `+ Successfully disabled inhibitor: ${name}`);
+    }
+    case "monitor": {
+      const monitor = client.messageMonitors.get(name);
+      if (!monitor) return msg.channel.sendCode("diff", `- I cannot find the monitor: ${name}`);
+      monitor.conf.enabled = false;
+      return msg.sendCode("diff", `+ Successfully disabled monitor: ${name}`);
+    }
+    case "command": {
+      const command = client.commands.get(name) || client.commands.get(client.aliases.has(name));
+      if (!command) return msg.sendCode("diff", `- I cannot find the command: ${name}`);
+      command.conf.enabled = false;
+      return msg.sendCode("diff", `+ Successfully disabled command: ${name}`);
+    }
+    default:
+      return msg.sendMessage("This will never happen");
   }
 };
 
