5e7695cbe82849295723a138c1e8d987a037b6b4
bdistin <bdistin@gmail.com>
2017-04-15 10:28:48 -0500

    missed a localization thing

--- events/message.js
+++ events/message.js
@@ -1,7 +1,6 @@
 exports.run = async (client, msg) => {
 	if (!client.ready) return;
 	await this.runMessageMonitors(client, msg);
-	client.i18n.use(msg.guildConf.lang);
 	if (!client.funcs.handleMessage(client, msg)) return;
 	const command = client.funcs.parseCommand(client, msg);
 	client.funcs.handleCommand(client, msg, command);
