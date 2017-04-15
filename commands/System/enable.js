bdeaa6ec23bc34c690b05990802a00ca7bf77dde
bdistin <bdistin@gmail.com>
2017-04-14 23:08:52 -0500

    fix enable lint

--- commands/System/enable.js
+++ commands/System/enable.js
@@ -1,26 +1,26 @@
 exports.run = (client, msg, [type, name]) => {
-	switch (type) {
-		case 'inhibitor': {
-			const inhibitor = client.commandInhibitors.get(name);
-			if (!inhibitor) return msg.channel.sendCode('diff', `- I cannot find the inhibitor: ${name}`);
-			inhibitor.conf.enabled = true;
-			return msg.sendCode('diff', `+ Successfully enabled inhibitor: ${name}`);
-		}
-		case 'monitor': {
-			const monitor = client.messageMonitors.get(name);
-			if (!monitor) return msg.channel.sendCode('diff', `- I cannot find the monitor: ${name}`);
-			monitor.conf.enabled = true;
-			return msg.sendCode('diff', `+ Successfully enabled monitor: ${name}`);
-		}
-		case 'command': {
-			const command = client.commands.get(name) || client.commands.get(client.aliases.has(name));
-			if (!command) return msg.sendCode('diff', `- I cannot find the command: ${name}`);
-			command.conf.enabled = true;
-			return msg.sendCode('diff', `+ Successfully enabled command: ${name}`);
-		}
-		default:
-			return msg.sendMessage('This will never happen');
-	}
+  switch (type) {
+    case 'inhibitor': {
+      const inhibitor = client.commandInhibitors.get(name);
+      if (!inhibitor) return msg.channel.sendCode("diff", `- I cannot find the inhibitor: ${name}`);
+      inhibitor.conf.enabled = true;
+      return msg.sendCode("diff", `+ Successfully enabled inhibitor: ${name}`);
+    }
+    case 'monitor': {
+      const monitor = client.messageMonitors.get(name);
+      if (!monitor) return msg.channel.sendCode("diff", `- I cannot find the monitor: ${name}`);
+      monitor.conf.enabled = true;
+      return msg.sendCode("diff", `+ Successfully enabled monitor: ${name}`);
+    }
+    case 'command': {
+      const command = client.commands.get(name) || client.commands.get(client.aliases.has(name));
+      if (!command) return msg.sendCode("diff", `- I cannot find the command: ${name}`);
+      command.conf.enabled = true;
+      return msg.sendCode("diff", `+ Successfully enabled command: ${name}`);
+    }
+    default:
+      return msg.sendMessage("This will never happen");
+    }
 };
 
 exports.conf = {
