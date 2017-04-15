cb1a6d9cd4e3e0cfedd2028f1cf7edc39ecae5b0
bdistin <bdistin@gmail.com>
2017-04-14 22:43:30 -0500

    enable/disable optimizations

--- commands/System/enable.js
+++ commands/System/enable.js
@@ -1,42 +1,25 @@
-/* eslint-disable no-case-declarations, consistent-return */
 exports.run = async (client, msg, [type, name]) => {
 	switch (type) {
-		case 'inhibitor':
-			let inhibitor;
-			if (client.commandInhibitors.has(name)) {
-				inhibitor = name;
-			}
-			if (!inhibitor) {
-				msg.channel.sendCode('diff', `- I cannot find the inhibitor: ${name}`);
-			}
-			client.commandInhibitors.get(inhibitor).conf.enabled = true;
-			msg.sendCode('diff', `+ Successfully enabled inhibitor: ${name}`);
-			break;
-		case 'monitor':
-			let monitor;
-			if (client.messageMonitors.has(name)) {
-				monitor = name;
-			}
-			if (!monitor) {
-				return msg.channel.sendCode('diff', `- I cannot find the monitor: ${name}`);
-			}
-			client.messageMonitors.get(monitor).conf.enabled = true;
-			msg.sendCode('diff', `+ Successfully enabled monitor: ${name}`);
-			break;
-		case 'command':
-			let command;
-			if (client.commands.has(name)) {
-				command = name;
-			} else if (client.aliases.has(name)) {
-				command = client.aliases.get(name);
-			}
-			if (!command) {
-				return msg.sendCode('diff', `- I cannot find the command: ${name}`);
-			}
-			client.commands.get(command).conf.enabled = true;
-			msg.sendCode('diff', `+ Successfully enabled command: ${name}`);
-			break;
-    // no default
+		case 'inhibitor': {
+			const inhibitor = client.commandInhibitors.get(name);
+			if (!inhibitor) return msg.channel.sendCode('diff', `- I cannot find the inhibitor: ${name}`);
+			inhibitor.conf.enabled = true;
+			return msg.sendCode('diff', `+ Successfully enabled inhibitor: ${name}`);
+		}
+		case 'monitor': {
+			const monitor = client.messageMonitors.get(name);
+			if (!monitor) return msg.channel.sendCode('diff', `- I cannot find the monitor: ${name}`);
+			monitor.conf.enabled = true;
+			return msg.sendCode('diff', `+ Successfully enabled monitor: ${name}`);
+		}
+		case 'command': {
+			const command = client.commands.get(name) || client.commands.get(client.aliases.has(name));
+			if (!command) return msg.sendCode('diff', `- I cannot find the command: ${name}`);
+			command.conf.enabled = true;
+			return msg.sendCode('diff', `+ Successfully enabled command: ${name}`);
+		}
+		default:
+			return msg.sendMessage('This will never happen');
 	}
 };
 
@@ -46,12 +29,12 @@ exports.conf = {
 	aliases: [],
 	permLevel: 10,
 	botPerms: [],
-	requiredFuncs: [],
+	requiredFuncs: []
 };
 
 exports.help = {
 	name: 'enable',
 	description: 'Re-enables or temporarily enables a Inhibitor/Command/Monitor. Default state restored on reboot.',
 	usage: '<type:str> <name:str>',
-	usageDelim: ' ',
+	usageDelim: ' '
 };
