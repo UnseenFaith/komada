e3854e54687d92a07de826fc8514187d2683f220
bdistin <bdistin@gmail.com>
2017-04-21 23:12:33 -0500

    cache and validate perm structure

--- inhibitors/permissions.js
+++ inhibitors/permissions.js
@@ -1,7 +1,27 @@
+let permStructure;
+
 exports.conf = {
 	enabled: true,
 	spamProtection: false,
 	priority: 10
+};
+
+exports.run = (client, msg, cmd) => {
+	for (let i = cmd.conf.permLevel; i < 11; i++) {
+		if (i === 11) return true;
+		if (permStructure[i].check(client, msg, cmd)) return false;
+		if (permStructure[i].break) break;
+	}
+	return 'You do not have permission to use this command.';
+};
+
+exports.init = (client) => {
+	permStructure = client.config.permStructure || this.default;
+	if (!Array.isArray(permStructure)) throw 'PermStructure must be an array.';
+	if (permStructure.some(perm => typeof perm !== 'object' || typeof perm.check !== 'function' || typeof perm.break !== 'boolean')) {
+		throw 'Perms must be an object with a check function and a break boolean.';
+	}
+	if (permStructure.length > 11) throw 'There can not be any permissions above 10.';
 };
 
 exports.default = [
@@ -69,13 +89,3 @@ exports.default = [
 		break: false
 	}
 ];
-
-exports.run = (client, msg, cmd) => {
-	const permStructure = client.config.permStructure || this.default;
-	for (let i = cmd.conf.permLevel; i < 11; i++) {
-		if (i === 11) return true;
-		if (permStructure[i].check(client, msg, cmd)) return false;
-		if (permStructure[i].break) break;
-	}
-	return 'You do not have permission to use this command.';
-};
