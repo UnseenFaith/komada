16eae6467cdf48d887ca480074329c53239130b7
bdistin <bdistin@gmail.com>
2017-05-27 09:31:26 -0500

    requested changes

--- classes/Resolver.js
+++ classes/Resolver.js
@@ -24,7 +24,7 @@ module.exports = class Resolver {
   async user(user) {
     if (user instanceof Discord.User) return user;
     if (user instanceof Discord.Member) return user.user;
-    if (user instanceof String) {
+    if (user.constructor.name === "String") {
       // eslint-disable-next-line no-nested-ternary
       return regex.userOrMember.test(user) ?
       this.client.user.bot ? this.client.fetchUser(regex.userOrMember.exec(user)[1]).catch(() => null) : this.client.users.get(regex.userOrMember.exec(user)[1]) :
@@ -35,38 +35,38 @@ module.exports = class Resolver {
 
   async member(member, guild) {
     if (member instanceof Discord.Member) return member;
-    if (member instanceof String) return regex.userOrMember.test(member) ? guild.fetchMember(regex.userOrMember.exec(member)[1]).catch(() => null) : null;
+    if (member.constructor.name === "String") return regex.userOrMember.test(member) ? guild.fetchMember(regex.userOrMember.exec(member)[1]).catch(() => null) : null;
     return null;
   }
 
   async channel(channel) {
     if (channel instanceof Discord.Channel) return channel;
-    if (channel instanceof String) return regex.channel.test(channel) ? this.client.channels.get(regex.channel.exec(channel)[1]) : null;
+    if (channel.constructor.name === "String") return regex.channel.test(channel) ? this.client.channels.get(regex.channel.exec(channel)[1]) : null;
     return null;
   }
 
   async guild(guild) {
     if (guild instanceof Discord.Channel) return guild;
-    if (guild instanceof String) return regex.snowflake.test(guild) ? this.client.guilds.get(guild) : null;
+    if (guild.constructor.name === "String") return regex.snowflake.test(guild) ? this.client.guilds.get(guild) : null;
     return null;
   }
 
   async role(role, guild) {
     if (role instanceof Discord.Channel) return role;
     if (role instanceof Discord.Role) return role;
-    if (role instanceof String) return regex.role.test(role) ? guild.roles.get(regex.role.exec(role)[1]) : null;
+    if (role.constructor.name === "String") return regex.role.test(role) ? guild.roles.get(regex.role.exec(role)[1]) : null;
     return null;
   }
 
   async boolean(bool) {
     if (bool instanceof Boolean) return bool;
-    if ([1, "true", "+", "t"].includes(bool)) return true;
-    if ([0, "false", "-", "f"].includes(bool)) return false;
+    if (["1", "true", "+", "t", "yes"].includes(String(bool).toLowerCase())) return true;
+    if (["0", "false", "-", "f", "no"].includes(String(bool).toLowerCase())) return false;
     return null;
   }
 
   async string(string) {
-    if (string instanceof String) return true;
+    if (string.constructor.name === "String") return string;
     return String(string);
   }
 
