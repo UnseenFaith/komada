111dc63d0fcfee9f3a76ae7ce49538ea8af325e7
Damian Blume <damian.blume@gmail.com>
2017-06-06 21:30:08 +0200

    Remove a dependancy in favor of node 8

--- events/log.js
+++ events/log.js
@@ -1,28 +1,32 @@
 const moment = require("moment");
 const chalk = require("chalk");
-const lpad = require("lpad");
 
 /* eslint-disable no-use-before-define */
 exports.run = (client, data, type = "log") => {
   data = resolveObject(data);
   let timestamp = "";
+
+  function padLines(text) {
+    return text.split("\n").forEach(str => str.padStart(timestamp.length + str, timestamp));
+  }
+
   if (!client.config.disableLogTimestamps) timestamp = moment().format("YYYY-MM-DD HH:mm:ss ");
   switch (type.toLowerCase()) {
     case "debug":
       if (!client.config.disableLogTimestamps) timestamp = chalk.bgMagenta(timestamp);
-      console.log(lpad(data, timestamp));
+      console.log(padLines(data));
       break;
     case "warn":
       if (!client.config.disableLogTimestamps) timestamp = chalk.black.bgYellow(timestamp);
-      console.log(lpad(data, timestamp));
+      console.log(padLines(data));
       break;
     case "error":
       if (!client.config.disableLogTimestamps) timestamp = chalk.bgRed(timestamp);
-      console.log(lpad(data, timestamp));
+      console.log(padLines(data));
       break;
     case "log":
       if (!client.config.disableLogTimestamps) timestamp = chalk.bgBlue(timestamp);
-      console.log(lpad(data, timestamp));
+      console.log(padLines(data));
       break;
       // no default
   }
