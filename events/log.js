8a90120f88dd31a6e947ac233914e45663e61f06
Damian Blume <damian.blume@gmail.com>
2017-06-06 22:12:54 +0200

    Fix and optimize log.js

--- events/log.js
+++ events/log.js
@@ -3,40 +3,34 @@ const chalk = require("chalk");
 
 /* eslint-disable no-use-before-define */
 exports.run = (client, data, type = "log") => {
-  data = resolveObject(data);
+  type = type.toLowerCase();
+
+  data = data.stack || data.message || data;
+  if (typeof data === "object" && typeof data !== "string" && !Array.isArray(data)) data = require("util").inspect(data, { depth: 0, colors: true });
+  if (Array.isArray(data)) data = data.join["\n"];
+
   let timestamp = "";
-
-  function padLines(text) {
-    return text.split("\n").forEach(str => str.padStart(timestamp.length + str, timestamp));
+  if (!client.config.disableLogTimestamps) {
+    timestamp = moment().format("YYYY-MM-DD HH:mm:ss ");
+    if (!client.config.disableLogColor) {
+      switch (type) {
+        case "debug":
+          timestamp = chalk.bgMagenta(timestamp);
+          break;
+        case "warn":
+          timestamp = chalk.black.bgYellow(timestamp);
+          break;
+        case "error":
+          timestamp = chalk.bgRed(timestamp);
+          break;
+        case "log":
+          timestamp = chalk.bgBlue(timestamp);
+          break;
+          // no default
+      }
+    }
   }
 
-  if (!client.config.disableLogTimestamps) timestamp = moment().format("YYYY-MM-DD HH:mm:ss ");
-  switch (type.toLowerCase()) {
-    case "debug":
-      if (!client.config.disableLogTimestamps && !client.config.disableLogColor) timestamp = chalk.bgMagenta(timestamp);
-      console.log(padLines(data));
-      break;
-    case "warn":
-      if (!client.config.disableLogTimestamps && !client.config.disableLogColor) timestamp = chalk.black.bgYellow(timestamp);
-      console.log(padLines(data));
-      break;
-    case "error":
-      if (!client.config.disableLogTimestamps && !client.config.disableLogColor) timestamp = chalk.bgRed(timestamp);
-      console.log(padLines(data));
-      break;
-    case "log":
-      if (!client.config.disableLogTimestamps && !client.config.disableLogColor) timestamp = chalk.bgBlue(timestamp);
-      console.log(padLines(data));
-      break;
-      // no default
-  }
+  if (type === "debug") type = "log";
+  console[type](data.split("\n").forEach(str => str.padStart(timestamp.length + str, timestamp)));
 };
-
-function resolveObject(error) {
-  error = error.stack || error.message || error;
-
-  if (typeof error === "object" && typeof error !== "string") {
-    return require("util").inspect(error, { depth: 0, colors: true });
-  }
-  return error;
-}
