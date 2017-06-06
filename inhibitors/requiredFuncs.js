401098d6a940391c578d110794286beecb2e02e9
Damian Blume <damian.blume@gmail.com>
2017-06-06 21:45:45 +0200

    Little clarification

--- inhibitors/requiredFuncs.js
+++ inhibitors/requiredFuncs.js
@@ -11,6 +11,6 @@ exports.run = (client, msg, cmd) => {
   cmd.conf.requiredFuncs.forEach((func) => {
     if (!client.funcs.hasOwnProperty(func)) funcs.push(func);
   });
-  if (funcs.length > 0) return `The client is missing **${funcs.join(", ")}**, and cannot run.`;
+  if (funcs.length > 0) return `The client is missing the **${funcs.join(", ")}** function and cannot run.`;
   return false;
 };
