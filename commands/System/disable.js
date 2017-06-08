fb1d03941378278606c086b36bf5b563f0f262a2
Damian Blume <damian.blume@gmail.com>
2017-06-09 00:38:33 +0200

    Make enable and disable much smaller

--- commands/System/disable.js
+++ commands/System/disable.js
@@ -1,33 +1,9 @@
 exports.run = async (client, msg, [type, name]) => {
-  function sendSuccess() { msg.sendCode("diff", `+ Successfully disabled ${type}: ${name}`); }
-  function sendError() { msg.sendCode("diff", `- I cannot find the ${type}: ${name}`); }
-  switch (type) {
-    case "command": {
-      const command = client.commands.get(name) || client.commands.get(client.aliases.has(name));
-      if (!command) return sendError();
-      command.conf.enabled = false;
-      return sendSuccess();
-    }
-    case "inhibitor": {
-      const inhibitor = client.commandInhibitors.get(name);
-      if (!inhibitor) return sendError();
-      inhibitor.conf.enabled = false;
-      return sendSuccess();
-    }
-    case "monitor": {
-      const monitor = client.messageMonitors.get(name);
-      if (!monitor) return sendError();
-      monitor.conf.enabled = false;
-      return sendSuccess();
-    }
-    case "finalizer": {
-      const finalizer = client.commandFinalizers.get(name);
-      if (!finalizer) return sendError();
-      finalizer.conf.enabled = false;
-      return sendSuccess();
-    }
-    // no default
-  }
+  const t = { command: "commands", inhibitor: "commandInhibitors", monitor: "messageMonitors", finalizer: "commandFinalizers" }[type];
+  const toDisable = client[t].get(name) || type === "command" ? client.commands.get(client.aliases.has(name)) : null;
+  if (!toDisable) return msg.sendCode("diff", `- I cannot find the ${type}: ${name}`);
+  toDisable.conf.enabled = false;
+  return msg.sendCode("diff", `+ Successfully disabled ${type}: ${name}`);
 };
 
 exports.conf = {
@@ -41,7 +17,7 @@ exports.conf = {
 
 exports.help = {
   name: "disable",
-  description: "Temporarily disables a command/inhibitor/monitor/finalizer. Default state restored on reboot.",
+  description: "Re-disables or temporarily disables a command/inhibitor/monitor/finalizer. Default state restored on reboot.",
   usage: "<command|inhibitor|monitor|finalizer> <name:str>",
   usageDelim: " ",
 };
--- commands/System/enable.js
+++ commands/System/enable.js
@@ -1,33 +1,9 @@
 exports.run = async (client, msg, [type, name]) => {
-  function sendSuccess() { msg.sendCode("diff", `+ Successfully enabled ${type}: ${name}`); }
-  function sendError() { msg.sendCode("diff", `- I cannot find the ${type}: ${name}`); }
-  switch (type) {
-    case "command": {
-      const command = client.commands.get(name) || client.commands.get(client.aliases.has(name));
-      if (!command) sendError();
-      command.conf.enabled = true;
-      return sendSuccess();
-    }
-    case "inhibitor": {
-      const inhibitor = client.commandInhibitors.get(name);
-      if (!inhibitor) sendError();
-      inhibitor.conf.enabled = true;
-      return sendSuccess();
-    }
-    case "monitor": {
-      const monitor = client.messageMonitors.get(name);
-      if (!monitor) sendError();
-      monitor.conf.enabled = true;
-      return sendSuccess();
-    }
-    case "finalizer": {
-      const finalizer = client.commandFinalizers.get(name);
-      if (!finalizer) sendError();
-      finalizer.conf.enabled = true;
-      return sendSuccess();
-    }
-    // no default
-  }
+  const t = { command: "commands", inhibitor: "commandInhibitors", monitor: "messageMonitors", finalizer: "commandFinalizers" }[type];
+  const toDisable = client[t].get(name) || type === "command" ? client.commands.get(client.aliases.has(name)) : null;
+  if (!toDisable) return msg.sendCode("diff", `- I cannot find the ${type}: ${name}`);
+  toDisable.conf.enabled = true;
+  return msg.sendCode("diff", `+ Successfully enabled ${type}: ${name}`);
 };
 
 exports.conf = {
