33b9bc84b5dc3e3cfa9e0cd8a7dacbb5b3fc5462
Damian Blume <damian.blume@gmail.com>
2017-06-08 23:57:02 +0200

    Update commandLogging.js
    Add option to disable colors and fix name case.

--- finalizers/commandlogging.js
+++ finalizers/commandlogging.js
@@ -1,14 +1,13 @@
 const chalk = require("chalk");
 const now = require("performance-now");
 
-const clk = new chalk.constructor({ enabled: true });
-
 exports.run = (client, msg, mes, start) => {
   if (client.config.cmdLogging) {
+    chalk.enabled = !client.config.disableLogColor;
     client.emit("log", [
       `${msg.cmdMsg.cmd.help.name}(${msg.cmdMsg.args.join(", ")})`,
-      msg.cmdMsg.reprompted ? `${clk.bgRed(`[${(now() - start).toFixed(2)}ms]`)}` : `${clk.bgBlue(`[${(now() - start).toFixed(2)}ms]`)}`,
-      `${clk.black.bgYellow(`${msg.author.username}[${msg.author.id}]`)}`,
+      msg.cmdMsg.reprompted ? `${chalk.bgRed(`[${(now() - start).toFixed(2)}ms]`)}` : `${chalk.bgBlue(`[${(now() - start).toFixed(2)}ms]`)}`,
+      `${chalk.black.bgYellow(`${msg.author.username}[${msg.author.id}]`)}`,
       this.channel(msg),
     ].join(" "), "log");
   }
@@ -17,11 +16,11 @@ exports.run = (client, msg, mes, start) => {
 exports.channel = (msg) => {
   switch (msg.channel.type) {
     case "text":
-      return `${clk.bgGreen(`${msg.guild.name}[${msg.guild.id}]`)}`;
+      return `${chalk.bgGreen(`${msg.guild.name}[${msg.guild.id}]`)}`;
     case "dm":
-      return `${clk.bgMagenta("Direct Messages")}`;
+      return `${chalk.bgMagenta("Direct Messages")}`;
     case "group":
-      return `${clk.bgCyan(`Group DM => ${msg.channel.owner.username}[${msg.channel.owner.id}]`)}`;
+      return `${chalk.bgCyan(`Group DM => ${msg.channel.owner.username}[${msg.channel.owner.id}]`)}`;
     default:
       return "not going to happen";
   }
