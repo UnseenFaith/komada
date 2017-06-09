863bca9310599f888cb98787f788eb99a91406f0
Damian Blume <damian.blume@gmail.com>
2017-06-09 09:15:18 +0200

    Throw an error instead of assuming

--- inhibitors/runIn.js
+++ inhibitors/runIn.js
@@ -5,7 +5,7 @@ exports.conf = {
 };
 
 exports.run = (client, msg, cmd) => {
-  if (!cmd.conf.runIn) cmd.conf.runIn = ["text"];
+  if (!cmd.conf.runIn) throw "This command is not configured to run in any channel.";
   if (cmd.conf.runIn.includes(msg.channel.type)) return false;
   return `This command is only avaliable in ${cmd.conf.runIn.join(" ")} channels`;
 };
