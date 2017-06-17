1126b566103309f8a64fc860a8eeb12af771b68c
CyberiumShadow <CyberiumShadow@users.noreply.github.com>
2017-06-18 01:40:27 +1000

    NPM Resync

--- utils/loadCommands.js
+++ utils/loadCommands.js
@@ -8,7 +8,7 @@ const loadCommands = (client, baseDir) => new Promise(async (res, rej) => {
     const files = await client.funcs.getFileListing(client, baseDir, "commands").catch(err => client.emit("error", client.funcs.newError(err)));
     const fn = files.map(f => client.funcs.loadSingleCommand(client, `${f.name}`, false, `${f.path}${sep}${f.base}`));
     await Promise.all(fn).catch(e => client.funcs.log(e, "error"));
-    resolve();
+    res();
   } catch (e) {
     if (e.code === "MODULE_NOT_FOUND") {
       const module = /'[^']+'/g.exec(e.toString());
