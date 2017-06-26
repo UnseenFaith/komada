a1a444039b4d8d5786b290a8e80ac55888bbbc7c
bdistin <bdistin@gmail.com>
2017-06-26 07:08:42 -0500

    Update requiredSettings.js

--- inhibitors/requiredSettings.js
+++ inhibitors/requiredSettings.js
@@ -6,7 +6,7 @@ exports.conf = {
 
 exports.run = (client, msg, cmd) => {
   if (!cmd.conf.requiredSettings || cmd.conf.requiredSettings.length === 0) return false;
-  const settings = cmd.conf.requiredSettings.filter(setting => msg.guildSettings[setting]);
+  const settings = cmd.conf.requiredSettings.filter(setting => !msg.guildSettings[setting]);
   if (settings.length > 0) return `The guild is missing the **${settings.join(", ")}** guild setting${settings.length > 1 ? "s" : ""} and cannot run.`;
   return false;
 };
