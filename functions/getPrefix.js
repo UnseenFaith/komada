ebce178b907557e4c0705c753d414e511406637d
kyraNET <kyranet@users.noreply.github.com>
2017-06-29 18:25:11 +0200

    Gotta be fast.

--- functions/getPrefix.js
+++ functions/getPrefix.js
@@ -1,14 +1,13 @@
 module.exports = async (client, msg) => {
   if (client.config.prefixMention.test(msg.content)) return client.config.prefixMention;
-  const config = await msg.guildSettings;
-  let { prefix } = config;
-  const escape = client.funcs.regExpEsc;
+  const { prefix } = msg.guildSettings;
+  const { regExpEsc } = client.funcs;
   if (prefix instanceof Array) {
-    prefix.forEach((pref) => {
-      if (msg.content.startsWith(pref)) prefix = pref;
-      else prefix = false;
-    });
+    for (let i = 0; i < prefix.length; i++) {
+      if (!msg.content.startsWith(prefix[i])) continue;
+      return new RegExp(`^${regExpEsc(prefix[i])}`);
+    }
   }
-  if (prefix && msg.content.startsWith(prefix)) return new RegExp(`^${escape(prefix)}`);
+  if (prefix && msg.content.startsWith(prefix)) return new RegExp(`^${regExpEsc(prefix)}`);
   return false;
 };
