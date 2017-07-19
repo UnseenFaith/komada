c618d5f1437d952e46ae7cd1432b6a3825cb5c55
Faith <tba28012@hotmail.com>
2017-07-19 18:24:40 -0400

    I think its finished

--- classes/settingGateway.js
+++ classes/settingGateway.js
@@ -20,7 +20,7 @@ module.exports = class SettingGateway extends SchemaManager {
   }
 
   /**
-   * Initialize the configuration for all Guilds.
+   * Initialize the configuration for this gateway.
    * @returns {void}
    */
   async init() {
@@ -41,34 +41,34 @@ module.exports = class SettingGateway extends SchemaManager {
   }
 
   /**
-   * Create a new Guild entry for the configuration.
-   * @param {Guild|Snowflake} guild The Guild object or snowflake.
+   * Create a new entry in the configuration.
+   * @param {Object|string} input An object containing a id property, like discord.js objects, or a string.
    * @returns {void}
    */
-  async create(guild) {
-    const target = await this.validate(guild);
-    await this.provider.create(this.type, target.id, this.defaults);
-    super.set(target.id, this.defaults);
+  async create(input) {
+    const target = await this.validate(input).then(output => (output.id || output));
+    await this.provider.create(this.type, target, this.defaults);
+    super.set(target, this.defaults);
   }
 
   /**
-   * Remove a Guild entry from the configuration.
-   * @param {Snowflake} guild The Guild object or snowflake.
+   * Remove an entry from the configuration.
+   * @param {string} input A key to delete from the cache.
    * @returns {void}
    */
-  async destroy(guild) {
-    await this.provider.delete(this.type, guild);
-    super.delete(this.type, guild);
+  async destroy(input) {
+    await this.provider.delete(this.type, input);
+    super.delete(this.type, input);
   }
 
   /**
-   * Get a Guild entry from the configuration.
-   * @param {(Guild|Snowflake)} guild The Guild object or snowflake.
+   * Get an entry from the cache.
+   * @param {string} input A key to get the value for.
    * @returns {Object}
    */
-  get(guild) {
-    if (guild === "default") return this.defaults;
-    return super.get(guild) || this.defaults;
+  get(input) {
+    if (input === "default") return this.defaults;
+    return super.get(input) || this.defaults;
   }
 
   /**
@@ -87,83 +87,83 @@ module.exports = class SettingGateway extends SchemaManager {
   }
 
   /**
-   * Sync either all Guild entries from the configuration, or a single one.
-   * @param {(Guild|Snowflake)} [guild=null] The configuration for the selected Guild, if specified.
+   * Sync either all entries from the configuration, or a single one.
+   * @param {Object|string} [input=null] An object containing a id property, like discord.js objects, or a string.
    * @returns {void}
    */
-  async sync(guild = null) {
-    if (!guild) {
+  async sync(input = null) {
+    if (!input) {
       const data = await this.provider.getAll(this.type);
       if (this.sql) for (let i = 0; i < data.length; i++) this.sql.deserializer(data[i]);
       for (const key of data) super.set(key.id, key);
       return;
     }
-    const target = await this.validate(guild);
+    const target = await this.validate(input).then(output => (output.id || output));
     const data = await this.provider.get(this.type, target.id);
     if (this.sql) this.sql.deserializer(data);
-    await super.set(target.id, data);
+    await super.set(target, data);
   }
 
   /**
-   * Reset a key's value to default from a Guild configuration.
-   * @param {(Guild|Snowflake)} guild The Guild object or snowflake.
+   * Reset a key's value to default from a entry.
+   * @param {Object|string} input An object containing a id property, like discord.js objects, or a string.
    * @param {string} key The key to reset.
    * @returns {*}
    */
-  async reset(guild, key) {
-    const target = await this.validate(guild);
+  async reset(input, key) {
+    const target = await this.validate(input).then(output => (output.id || output));
     if (!(key in this.schema)) throw `The key ${key} does not exist in the current data schema.`;
     const defaultKey = this.schema[key].default;
-    await this.provider.update(this.type, target.id, { [key]: defaultKey });
-    this.sync(target.id);
+    await this.provider.update(this.type, target, { [key]: defaultKey });
+    this.sync(target);
     return defaultKey;
   }
 
   /**
-   * Update a Guild's configuration.
-   * @param {(Guild|Snowflake)} guild The Guild object or snowflake.
+   * Updates an entry.
+   * @param {Object|string} input An object containing a id property, like discord.js objects, or a string.
    * @param {string} key The key to update.
    * @param {any} data The new value for the key.
    * @returns {any}
    */
-  async update(guild, key, data) {
+  async update(input, key, data) {
     if (!(key in this.schema)) throw `The key ${key} does not exist in the current data schema.`;
-    const target = await this.validate(guild);
-    let result = await this.resolver[this.schema[key].type.toLowerCase()](data, target, this.schema[key]);
+    const target = await this.validate(input).then(output => (output.id || output));
+    let result = await this.resolver[this.schema[key].type.toLowerCase()](data, this.client.guilds.get(target), this.schema[key]);
     if (result.id) result = result.id;
-    await this.provider.update(this.type, target.id, { [key]: result });
+    await this.provider.update(this.type, target, { [key]: result });
     await this.sync(target.id);
     return result;
   }
 
   /**
    * Update an array from the a Guild's configuration.
-   * @param {(Guild|Snowflake)} guild The Guild object or snowflake.
+   * @param {Object|string} input An object containing a id property, like discord.js objects, or a string.
    * @param {string} type Either 'add' or 'remove'.
    * @param {string} key The key from the Schema.
    * @param {any} data The value to be added or removed.
    * @returns {boolean}
    */
-  async updateArray(guild, type, key, data) {
+  async updateArray(input, type, key, data) {
     if (!["add", "remove"].includes(type)) throw "The type parameter must be either add or remove.";
     if (!(key in this.schema)) throw `The key ${key} does not exist in the current data schema.`;
     if (!this.schema[key].array) throw `The key ${key} is not an Array.`;
     if (data === undefined) throw "You must specify the value to add or filter.";
-    const target = await this.validate(guild);
-    let result = await this.resolver[this.schema[key].type.toLowerCase()](data, target, this.schema[key]);
+    const target = await this.validate(input).then(output => (output.id || output));
+    let result = await this.resolver[this.schema[key].type.toLowerCase()](data, this.client.guilds.get(target), this.schema[key]);
     if (result.id) result = result.id;
-    const cache = this.get(target.id);
+    const cache = this.get(target);
     if (type === "add") {
       if (cache[key].includes(result)) throw `The value ${data} for the key ${key} already exists.`;
       cache[key].push(result);
-      await this.provider.update(this.type, target.id, { [key]: cache[key] });
+      await this.provider.update(this.type, target, { [key]: cache[key] });
       await this.sync(target.id);
       return result;
     }
     if (!cache[key].includes(result)) throw `The value ${data} for the key ${key} does not exist.`;
     cache[key] = cache[key].filter(v => v !== result);
-    await this.provider.update(this.type, target.id, { [key]: cache[key] });
-    await this.sync(target.id);
+    await this.provider.update(this.type, target, { [key]: cache[key] });
+    await this.sync(target);
     return true;
   }
 
