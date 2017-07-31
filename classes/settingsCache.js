8dd8742a3b6f19d3e346efe42a2c048256ef6390
kyraNET <kyranet@users.noreply.github.com>
2017-07-31 18:48:05 +0200

    Update settingsCache.js

--- classes/settingsCache.js
+++ classes/settingsCache.js
@@ -6,22 +6,22 @@ class SettingsCache {
     Object.defineProperty(this, "client", { value: client });
     this.resolver = new SettingResolver(client);
 
-    this.guilds = new SettingGateway(this, "guilds", this.validate.bind(null, this), this.defaultDataSchema);
+    this.guilds = new SettingGateway(this, "guilds", this.validate.bind(null, this.resolver), this.defaultDataSchema);
   }
 
   async add(name, validateFunction, schema = {}) {
     if (!name || typeof name !== "string") throw "You must pass a name for your new gateway and it must be a string.";
     if (name in this) throw "There is already a Gateway with that name.";
     if (typeof validateFunction !== "function") throw "You must pass a validate function.";
-    validateFunction = validateFunction.bind(null, this);
+    validateFunction = validateFunction.bind(null, this.resolver);
     if (schema.constructor.name !== "Object") throw "Schema must be a valid object or left undefined for an empty object.";
     this[name] = new SettingGateway(this, name, validateFunction, schema);
     await this[name].init();
     return this[name];
   }
 
-  async validate(cache, guild) { // eslint-disable-line
-    const result = await cache.resolver.guild(guild);
+  async validate(resolver, guild) { // eslint-disable-line
+    const result = await resolver.guild(guild);
     if (!result) throw "The parameter <Guild> expects either a Guild ID or a Guild Object.";
     return result;
   }
