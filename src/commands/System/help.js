2c3d69870f780b714ee929a7fe836c006d7c877f
Johnson Chen <CyberiumShadow@users.noreply.github.com>
2017-08-21 12:11:51 +1000

    \u200b Fix (#339)

--- src/commands/System/help.js
+++ src/commands/System/help.js
@@ -57,7 +57,7 @@ exports.buildHelp = (client, msg) => {
       const subcat = command.help.subCategory;
       if (!help.hasOwnProperty(cat)) help[cat] = {};
       if (!help[cat].hasOwnProperty(subcat)) help[cat][subcat] = [];
-      help[cat][subcat].push(`${prefix}${command.help.name.padEnd(longest)} :: ${command.help.description}`);
+      help[cat][subcat].push(`\u200b${prefix}${command.help.name.padEnd(longest)} :: ${command.help.description}`);
     }
   }
 
