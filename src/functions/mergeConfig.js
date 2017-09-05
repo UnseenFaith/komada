46017efa2a8725ef551c615582138926bbae7dcb
Faith <tba28012@hotmail.com>
2017-09-05 08:58:19 +0200

    final fixes

--- src/functions/mergeConfig.js
+++ src/functions/mergeConfig.js
@@ -18,7 +18,7 @@ const defaultPermStructure = new PermLevels()
 
 
 module.exports = (options) => {
-  for (const key in Object.keys(this.DEFAULT_OPTIONS)) { // eslint-disable-line
+  for (const key in this.DEFAULT_OPTIONS) { // eslint-disable-line
     if (!(key in options)) options[key] = this.DEFAULT_OPTIONS[key];
   }
   this.validate(options);
@@ -56,7 +56,7 @@ exports.DEFAULT_OPTIONS = {
 exports.validate = (options) => {
   const pieces = Object.keys(this.DEFAULT_OPTIONS.disabled);
   if ("prefix" in options && typeof options.prefix !== "string") throw new TypeError("Prefix must be a string value.");
-  if ("ownerID" in options && (typeof options.ownerID !== "string" || options.ownerID !== null)) throw new TypeError("OwnerID must be a string (user id) if provided.");
+  if ("ownerID" in options && typeof options.ownerID !== "string" && options.ownerID !== null) throw new TypeError("OwnerID must be a string (user id) if provided.");
   if ("disabled" in options) {
     if (typeof options.disabled !== "object" || Array.isArray(options.disabled)) throw new TypeError("Disabled must be a valid object");
     for (const key of Object.keys(options.disabled)) { // eslint-disable-line
@@ -65,10 +65,10 @@ exports.validate = (options) => {
     }
   }
   if ("permStructure" in options) {
-    if (options.permStructure instanceof PermLevels || !Array.isArray(options.permStructure)) throw new TypeError("PermStructure must be a valid array with 11 entries, or a instance of Komada.PermLevels");
+    if (options.permStructure.constructor.name !== "PermissionLevels" && !Array.isArray(options.permStructure)) throw new TypeError("PermStructure must be a valid array with 11 entries, or a instance of Komada.PermLevels");
   }
   if ("selfbot" in options && typeof options.selfbot !== "boolean") throw new TypeError("Selfbot must be true or false.");
-  if ("readyMessage" in options && typeof "readyMessage" !== "function") throw new TypeError("ReadyMessage must be a function.");
+  if ("readyMessage" in options && typeof options.readyMessage !== "function") throw new TypeError("ReadyMessage must be a function.");
   if ("commandMessageLifetime" in options && typeof options.commandMessageLifetime !== "number") throw new TypeError("CommandMessageLifetime must be a number.");
   if ("commandMessageSweep" in options && typeof options.commandMessageSweep !== "number") throw new TypeError("CommandMessageSweep must be a number.");
   if ("disableLogTimestamps" in options && typeof options.disableLogTimestamps !== "boolean") throw new TypeError("DisableLogTimestamps must be true or false.");
