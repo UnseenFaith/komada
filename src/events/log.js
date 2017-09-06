f401d1da6ca6da9bd633825362b5e5c31b78a7ad
HellPie <hellpiedev+git@gmail.com>
2017-09-06 14:21:40 +0200

    Redesigned log output
    - Added "What a Terrible Failure" and "Verbose" log types
    - Recolored logs
    - Added shortcut events for verbose log type

--- src/events/log.js
+++ src/events/log.js
@@ -2,6 +2,14 @@ const moment = require("moment");
 const chalk = require("chalk");
 
 const clk = new chalk.constructor({ enabled: true });
+const formats = {
+  verbose: { time: clk.grey, msg: clk.grey, logger: 'log' },
+  debug: { time: clk.magenta, logger: 'log' },
+  log: { time: clk.blue },
+  warn: { time: clk.black.bgYellow },
+  error: { time: clk.bgRed },
+  wtf: { time: clk.bold.underline.bgRed, msg: clk.bold.underline.bgRed, logger: 'error' }
+};
 
 /* eslint-disable no-use-before-define */
 exports.run = (client, data, type = "log") => {
@@ -11,29 +19,8 @@ exports.run = (client, data, type = "log") => {
   if (typeof data === "object" && typeof data !== "string" && !Array.isArray(data)) data = require("util").inspect(data, { depth: 0, colors: true });
   if (Array.isArray(data)) data = data.join["\n"];
 
-  let timestamp = "";
-  if (!client.config.disableLogTimestamps) {
-    timestamp = `[${moment().format("YYYY-MM-DD HH:mm:ss")}]`;
-    if (!client.config.disableLogColor) {
-      switch (type) {
-        case "debug":
-          timestamp = clk.bgMagenta(timestamp);
-          break;
-        case "warn":
-          timestamp = clk.black.bgYellow(timestamp);
-          break;
-        case "error":
-          timestamp = clk.bgRed(timestamp);
-          break;
-        case "log":
-          timestamp = clk.bgBlue(timestamp);
-          break;
-          // no default
-      }
-    }
-    timestamp += " ";
-  }
-
-  if (type === "debug") type = "log";
-  console[type](data.split("\n").map(str => timestamp + str).join("\n"));
+  const format = formats[type || "log"];
+  let time = client.config.disableLogTimestamps ? "" : `[${moment().format("YYYY-MM-DD HH:mm:ss")}]`;
+  if ("time" in format) time = format.type(time);
+  console[format.logger || "log"](data.split("\n").map(str => `${time}${format.msg && !client.config.disableLogColor ? format.msg(time ? ` ${str}` : str) : time ? ` ${str}` : str}`).join("\n"));
 };
