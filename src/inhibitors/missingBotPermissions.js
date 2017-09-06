e97fab7b6ecaa3b6693d2fba3a3f3810b43647c6
Faith <tba28012@hotmail.com>
2017-09-06 10:09:49 +0200

    fix missing-perms

--- src/inhibitors/missingBotPermissions.js
+++ src/inhibitors/missingBotPermissions.js
@@ -20,6 +20,12 @@ exports.conf = {
 
 exports.run = (client, msg, cmd) => {
   const missing = msg.channel.type === "text" ? msg.channel.permissionsFor(client.user).missing(cmd.conf.botPerms) : impliedPermissions.missing(cmd.conf.botPerms);
-  if (missing.length > 0) return `Insufficient permissions, missing: **${client.funcs.toTitleCase(missing.join(", ").split("_").join(" "))}**`;
+  if (missing.length > 0) {
+    if (missing.includes("READ_MESSAGE_HISTORY") || missing.includes("SEND_MESSAGES")) {
+      client.emit("log", `Unable to View Message history or Send Messages in Channel[${msg.channel.id}](${msg.channel.name})`, "error");
+      return true;
+    }
+    return `Insufficient permissions, missing: **${client.funcs.toTitleCase(missing.join(", ").split("_").join(" "))}**`;
+  }
   return false;
 };
