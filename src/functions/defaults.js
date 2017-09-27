7f002a31a6b8799e49345b50e651cd96ae73b456
Faith <tba28012@hotmail.com>
2017-09-27 19:41:08 +0200

    fix other exports being dropped

--- src/functions/defaults.js
+++ src/functions/defaults.js
@@ -1,24 +1,24 @@
-exports.command = ({ conf, help, run }, file) => {
+exports.command = (cmd, file) => {
   const name = file.slice(-1)[0].slice(0, -3);
   const fullCategory = file.slice(0, -1);
-  conf = {
-    enabled: conf.enabled || true,
-    runIn: conf.runIn || ["text", "dm", "group"],
-    aliases: conf.aliases || [],
-    permLevel: conf.permLevel || 0,
-    botPerms: conf.botPerms || [],
-    requiredFuncs: conf.requiredFuncs || [],
-    requiredSettings: conf.requiredSettings || [],
+  cmd.conf = {
+    enabled: cmd.conf.enabled || true,
+    runIn: cmd.conf.runIn || ["text", "dm", "group"],
+    aliases: cmd.conf.aliases || [],
+    permLevel: cmd.conf.permLevel || 0,
+    botPerms: cmd.conf.botPerms || [],
+    requiredFuncs: cmd.conf.requiredFuncs || [],
+    requiredSettings: cmd.conf.requiredSettings || [],
   };
-  help = {
-    name: help.name || name,
-    description: help.description || "No Description provided.",
-    usage: help.usage || "",
-    usageDelim: help.usageDelim || "",
-    extendedHelp: help.extendedHelp || "",
+  cmd.help = {
+    name: cmd.help.name || name,
+    description: cmd.help.description || "No Description provided.",
+    usage: cmd.help.usage || "",
+    usageDelim: cmd.help.usageDelim || "",
+    extendedHelp: cmd.help.extendedHelp || "",
     fullCategory,
     category: fullCategory[0] || "General",
     subCategory: fullCategory[1] || "General",
   };
-  return { conf, help, run };
+  return cmd;
 };
