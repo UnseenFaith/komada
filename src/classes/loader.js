3a95930f6db74e15557c174a21c7b09b336fb6fc
Faith <tba28012@hotmail.com>
2017-12-04 22:14:37 +0100

    Remove a tiny bit of code duplication

--- src/classes/loader.js
+++ src/classes/loader.js
@@ -73,6 +73,14 @@ class Loader {
     }
   }
 
+  async _reload(dir, name) {
+    const file = name.endsWith(".js") ? name : `${name}.js`;
+    const files = await this._traverse(dir);
+    const piece = files.filter(([, f]) => f === file)[0];
+    if (!piece) throw `Could not find a reloadable file named ${file.slice(0, -3)}`;
+    return { file, piece };
+  }
+
   /** FUNCTIONS */
 
   async _loadFunctions() {
@@ -92,10 +100,7 @@ class Loader {
   }
 
   async _reloadFunction(name) {
-    const file = name.endsWith(".js") ? name : `${name}.js`;
-    const files = await this._traverse(this.clientDirs.functions);
-    const func = files.filter(([, f]) => f === file)[0];
-    if (!func) throw `Could not find a reloadable file named ${file.slice(0, -3)}`;
+    const { piece: func, file } = await this._reload(this.clientDirs.functions, name);
     if (this[file.slice(-3)]) delete this[file.slice(0, -3)];
     const fun = this._loadFunction(func);
     if (fun.init) fun.init(this.client);
@@ -122,10 +127,7 @@ class Loader {
   }
 
   async _reloadEvent(name) {
-    const file = name.endsWith(".js") ? name : `${name}.js`;
-    const files = await this._traverse(this.clientDirs.events);
-    const evt = files.filter(([, f]) => f === file)[0];
-    if (!evt) throw `Could not find a reloadable file named ${file.slice(0, -3)}`;
+    const { piece: evt, file } = await this._reload(this.clientDirs.events, name);
     const listener = this.client.eventHandlers.get(file.slice(0, -3));
     if (listener) this.client.removeListener(name, listener);
     this._loadFunction(evt);
@@ -198,10 +200,7 @@ class Loader {
   }
 
   async _reloadInhibitor(name) {
-    const file = name.endsWith(".js") ? name : `${name}.js`;
-    const files = await this._traverse(this.clientDirs.inhibitors);
-    const inhibitor = files.filter(([, f]) => f === file)[0];
-    if (!inhibitor) throw `Could not find a reloadable file named ${file.slice(0, -3)}`;
+    const { piece: inhibitor, file } = await this._reload(this.clientDirs.inhibitors, name);
     const inhibit = this.client.commandInhibitors.get(file.slice(0, -3));
     if (inhibit) this.client.commandInhibitors.delete(file.slice(0, -3));
     const inhib = this._loadInhibitor(inhibitor);
@@ -229,10 +228,7 @@ class Loader {
   }
 
   async _reloadFinalizer(name) {
-    const file = name.endsWith(".js") ? name : `${name}.js`;
-    const files = await this._traverse(this.clientDirs.finalizers);
-    const final = files.filter(([, f]) => f === file)[0];
-    if (!final) throw `Could not find a reloadable file named ${file.slice(0, 3)}`;
+    const { piece: final, file } = await this._reload(this.clientDirs.finalizers, name);
     const finale = this.client.commandFinalizers.get(file.slice(0, -3));
     if (finale) this.client.commandFinalizers.delete(file.slice(0, -3));
     const finalizer = this._loadFinalizer(final);
@@ -260,10 +256,7 @@ class Loader {
   }
 
   async _reloadMonitor(name) {
-    const file = name.endsWith(".js") ? name : `${name}.js`;
-    const files = await this._traverse(this.clientDirs.monitors);
-    const mon = files.filter(([, f]) => f === file)[0];
-    if (!mon) throw `Could not find a reloadable file named ${file.slice(0, 3)}`;
+    const { piece: mon, file } = await this._reload(this.clientDirs.monitors, name);
     const monit = this.client.messageMonitors.get(file.slice(0, -3));
     if (monit) this.client.messageMonitors.delete(file.slice(0, -3));
     const monitor = this._loadMonitor(mon);
@@ -290,10 +283,7 @@ class Loader {
   }
 
   async _reloadProvider(name) {
-    const file = name.endsWith(".js") ? name : `${name}.js`;
-    const files = await this._traverse(this.clientDirs.inhibitors);
-    const pro = files.filter(([, f]) => f === file)[0];
-    if (!pro) throw `Could not find a reloadable file named ${file.slice(0, 3)}`;
+    const { piece: pro, file } = await this._reload(this.clientDirs.providers, name);
     const provide = this.client.providers.get(file.slice(0, -3));
     if (provide) this.client.providers.delete(file.slice(0, -3));
     const provider = this._loadProvider(pro);
