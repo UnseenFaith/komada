99a7734ca4bab5369a043ea76c0f3c33cdefe2a9
Faith <tba28012@hotmail.com>
2017-12-04 13:07:41 +0100

    Remove argument prompting stuff

--- src/structures/KomadaMessage.js
+++ src/structures/KomadaMessage.js
@@ -173,11 +173,9 @@ class KomadaMessage extends Message {
       return this.validateArgs();
     } else if (possible >= this._currentUsage.possibles.length) {
       if (this._currentUsage.type === "optional" && !this._repeat) {
-        this.args.splice(this.params.length, 0, undefined);
         this.params.push(undefined);
         return this.validateArgs();
       }
-      this.args.splice(this.params.length, 1, null);
       throw this.client.funcs.newError(`Your option didn't match any of the possibilities: (${this._currentUsage.possibles.map(poss => poss.name).join(", ")})`, 1);
     } else if (this.client.argResolver[this._currentUsage.possibles[possible].type]) {
       return this.client.argResolver[this._currentUsage.possibles[possible].type](this.args[this.params.length], this._currentUsage, possible, this._repeat, this.msg)
