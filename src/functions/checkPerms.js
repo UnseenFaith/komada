d0faa12ead715d55c7d82670201d949f40be6475
Faith <tba28012@hotmail.com>
2018-02-18 13:17:21 -0500

    fix logic error

--- src/functions/checkPerms.js
+++ src/functions/checkPerms.js
@@ -1,5 +1,5 @@
 module.exports = (client, msg, min) => {
-  for (let i = min; i < client.permStructure.size; i++) {
+  for (let i = min; i <= client.permStructure.size; i++) {
     if (client.permStructure.levels[i].check(client, msg)) return true;
     if (client.permStructure.levels[i].break) return false;
   }
